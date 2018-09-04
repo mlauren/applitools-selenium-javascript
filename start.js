3249d1f6c33756a2ea8a032fffe2bc3c2f4cf694
Raja Rao DV <rajaraodv100@yahoo.com>
2018-09-03 23:23:39 -0700

    first commit

---
+++ start.js
@@ -0,0 +1,76 @@
+require("chromedriver");
+
+var webdriver = require("selenium-webdriver");
+var Capabilities = webdriver.Capabilities;
+var Builder = webdriver.Builder;
+var By = webdriver.By;
+
+var SeleniumSDK = require("eyes.selenium");
+var Eyes = SeleniumSDK.Eyes;
+var ConsoleLogHandler = SeleniumSDK.ConsoleLogHandler;
+
+//Runs different tests based on CLI input such as "part1", "part2" and so on.
+var testSelector = require("./testSelector.js");
+console.log(testSelector);
+
+// Open a Chrome browser.
+var driver = new Builder().withCapabilities(Capabilities.chrome()).build();
+
+// Initialize the eyes SDK and set your private API key.
+var eyes = new Eyes();
+
+//⚠️️️  Please set the APPLITOOLS_API_KEY environment variable
+//on Mac: export APPLITOOLS_API_KEY='YOUR_API_KEY'
+//on windows: set APPLITOOLS_API_KEY='YOUR_API_KEY'
+//Note: You can get your API key by logging into Applitools | Click on the person icon (top-right corner) | Click on the "My API key" menu
+eyes.setApiKey(process.env.APPLITOOLS_API_KEY);
+
+//scroll the entire page
+eyes.setForceFullPageScreenshot(true);
+
+if (!process.env.APPLITOOLS_API_KEY) {
+  console.log(`
+     ⚠️️️  Please set the APPLITOOLS_API_KEY environment variable
+
+        * On Mac: export APPLITOOLS_API_KEY='YOUR_API_KEY'
+        
+        * On windows: set APPLITOOLS_API_KEY='YOUR_API_KEY'
+        
+        * Please Note: You can get your API key by logging into applitools.com | Click on the person icon (top-right corner) | Click on the "My API key" menu
+    `);
+  process.exit(0);
+}
+
+//eyes.setLogHandler(new ConsoleLogHandler(false));
+
+try {
+  // Start the test and set the browser's viewport size to 800x600.
+  eyes.open(driver, testSelector.appName, testSelector.testName, {
+    width: testSelector.viewportWidth,
+    height: testSelector.viewportHeight
+  });
+
+  // Navigate the browser to the "hello world!" web-site.
+  driver.get(testSelector.url);
+
+  // Visual checkpoint #1.
+  eyes.checkWindow(testSelector.windowName);
+
+  //Only go to the main app page if the test/tutorial needs it
+  if (testSelector.goto2ndPage) {
+    // Click the "Click me!" button.
+    driver.findElement(By.id("log-in")).click();
+
+    // Visual checkpoint #2.
+    eyes.checkWindow("Click!");
+  }
+
+  // End the test.
+  eyes.close(false);
+} finally {
+  // Close the browser.
+  driver.quit();
+
+  // If the test was aborted before eyes.close was called ends the test as aborted.
+  eyes.abortIfNotClosed();
+}
---
+++ testSelector.js
@@ -0,0 +1,54 @@
+const testname = process.argv[2];
+
+//var baseUrl = "https://pensive-bose-78b68c.netlify.com/";
+var baseUrl = "http://localhost:8000/";
+
+//default for "part1"
+let testsToRun = {
+  url: baseUrl,
+  appName: "Demo app",
+  windowName: "Login Page",
+  testName: "Login Page",
+  goto2ndPage: false,
+  viewportWidth: 1200,
+  viewportHeight: 750
+};
+
+if (testname === "part2") {
+  testsToRun = {
+    url: baseUrl + "index_v2.html",
+    appName: "Demo app",
+    windowName: "Login Page",
+    testName: "Login Page",
+    goto2ndPage: false,
+    viewportWidth: 1200,
+    viewportHeight: 750
+  };
+}
+
+//create baseline for the app.html
+if (testname === "part_5_1") {
+  testsToRun = {
+    url: baseUrl + "app.html",
+    appName: "Demo app",
+    windowName: "Main app page",
+    testName: "Main app page test",
+    goto2ndPage: false,
+    viewportWidth: 1400,
+    viewportHeight: 750
+  };
+}
+
+//Run tests on version 2 of the app(app_v2.html)
+if (testname === "part_5_2") {
+  testsToRun = {
+    url: baseUrl + "app_v2.html",
+    appName: "Demo app",
+    windowName: "Main app page",
+    testName: "Main app page test",
+    goto2ndPage: false,
+    viewportWidth: 1400,
+    viewportHeight: 750
+  };
+}
+module.exports = testsToRun;
